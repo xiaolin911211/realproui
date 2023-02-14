bc1eebfe479a35aca4f4fb451603b2238c869aed
xiaolin911211 <49163395+xiaolin911211@users.noreply.github.com>
2023-02-13 21:42:43 -0500

    Delete _static directory

--- _static/_next/static/chunks/pages/order-0cfa5f0e2ff698f3.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[941],{18625:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/order",function(){return u(42806)}])}},function(n){n.O(0,[806,774,888,179],(function(){return _=18625,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
